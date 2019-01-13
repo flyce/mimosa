8fe789511efa5c8fc1b86b5091810d4dbe149d4d
Echo <mail@flyce.cn>
2019-01-13 18:14:03 +0800

    fix URI

--- src/Config/env.js
+++ src/Config/env.js
@@ -1,6 +1,6 @@
 const config = {
-    url: 'http://10.1.11.187:4000/',
-    socket: 'ws://10.1.11.187:4000/',
+    url: 'http://10.1.47.197:4000/',
+    socket: 'ws://10.1.47.197:4000/',
     __DEV__: true,
     loginEffect: 7200,
 };
